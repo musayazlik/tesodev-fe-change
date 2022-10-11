4230e858aba32c9c0e8c4cf721e84fae1304204d
Musa Yazlik <musayazlik1997@gmail.com>
2022-10-11 14:54:17 +0300

    Main page design has been edited.

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -3,6 +3,7 @@ import { useNavigate, Link } from 'react-router-dom'
 
 import '../assets/css/mainPage.css'
 import MainPageLogo from '../assets/img/HomeLogo.png'
+import FooterImg from '../assets/img/FooterImg.png'
 
 const MainPage = () => {
   const navigate = useNavigate()
@@ -26,6 +27,27 @@ const MainPage = () => {
           <span className='logo-text position-relative'>Search app</span>
         </div>
       </main>
+      <footer>
+        <div className='container'>
+          <div className='row'>
+            <div className='column-1'>
+              <img src={FooterImg} alt=' Footer Image' />
+            </div>
+            <div className='column-2'>
+              <address>
+                <h3>İletişim</h3>
+                <p>Adres: Çifte Havuzlar Mah. Eski Londra Asfaltı Cad. Kuluçka Merkezi D2 Blok No: 151/1F İç Kapı No: 2B03</p>
+                <strong>
+                  Email: <a href='mailto:bilgi@tesodev.com'>bilgi@tesodev.com</a>
+                </strong>
+              </address>
+            </div>
+            <div className='column-3'>
+              <div id='map'></div>
+            </div>
+          </div>
+        </div>
+      </footer>
     </>
   )
 }

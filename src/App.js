9181d4dfca5161fd7abf03aaf78d8d965cd51e95
Musa Yazlik <musayazlik1997@gmail.com>
2022-10-16 21:27:45 +0300

    Clean coding has been done.

--- src/App.js
+++ src/App.js
@@ -1,35 +1,42 @@
-import { Route, Routes } from 'react-router';
-import { useState } from 'react';
-import MainPage from './pages/MainPage';
-import ListPage from './pages/ListPage';
-import AddLinkPage from './pages/AddLinkPage';
+import { Route, Routes } from 'react-router'
+
+import { useState } from 'react'
+
+import Context from './context/Context'
+
+import MainPage from './pages/MainPage'
+
+import ListPage from './pages/ListPage'
+
+import AddLinkPage from './pages/AddLinkPage'
 
 function App() {
-  const [data, setData] = useState([]);
-  const [searchValue, setSearchValue] = useState('');
-  const [dataFilter, setDataFilter] = useState([]);
+  const [data, setData] = useState([])
+  const [searchValue, setSearchValue] = useState('')
+  const [dataFilter, setDataFilter] = useState([])
+  const [status, setStatus] = useState({ button: false, dropdown: false })
+  const Store = {
+    data,
+    setData,
+    searchValue,
+    setSearchValue,
+    dataFilter,
+    setDataFilter,
+    status,
+    setStatus
+  }
 
   return (
     <>
-      <Routes path='/' element={<MainPage />}>
-        <Route
-          index
-          element={
-            <MainPage
-              data={data}
-              setData={setData}
-              searchValue={searchValue}
-              setSearchValue={setSearchValue}
-              dataFilter={dataFilter}
-              setDataFilter={setDataFilter}
-            />
-          }
-        />
-        <Route path='list' element={<ListPage />} />
-        <Route path='add' element={<AddLinkPage />} />
-      </Routes>
+      <Context.Provider value={Store}>
+        <Routes path='/' element={<MainPage />}>
+          <Route index element={<MainPage />} />
+          <Route path='list' element={<ListPage />} />
+          <Route path='add' element={<AddLinkPage />} />
+        </Routes>
+      </Context.Provider>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
